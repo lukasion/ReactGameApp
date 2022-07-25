29d1d02219899f00c8dfef2d09fbe7b44b93dad9
ellukasion <lukasion@gmail.com>
2022-07-25 21:43:12 +0200

    Change from component oriented to functional

--- src/index.js
+++ src/index.js
@@ -1,136 +1,77 @@
-import React from 'react';
+import {React, useState} from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.scss';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { FormGroup } from '@mui/material';
+import CryptoJS from 'crypto-js';
 
-class Square extends React.Component {
+const App = () => {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
 
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            value: null,
-        };
+    const tryLogin = event => {
+        fetch('http://localhost:3001/api/user', {
+            method: 'POST', // or 'PUT'
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                login: login,
+                password: CryptoJS.MD5(password).toString()
+            }),
+        }).then(response => response.json())
+        .then((res) => {
+            if (res.result) {
+
+            }
+        })
     }
 
+    const handleChangeLogin = event => {
+        setLogin(event.target.value);
+    };    
 
-    render() {
-        return (
-            <button className="square" onClick={() => {
-                if (this.state.value == null) {
-                    this.props.onClick(); 
-                    this.clicked();
-                }
-            }}>
-                {this.state.value}
-            </button>
-        );
-    }
+    const handleChangePassword = event => {
+        setPassword(event.target.value);
+    };    
 
-    clicked = (e)  => {
-        this.setState({value: this.props.currentPlayer})
-        this.props.changeCurrentPlayer(this.props.currentPlayer == 'X' ? 'O' : 'X');
-    }
-  }
-  
-class Board extends React.Component {
-    constructor (props) {
-        super(props)
-
-        this.state = {
-            currentPlayer: 'X',
-            squares: Array(9).fill(null)
-        }
-    }
-
-    changeCurrentPlayer(value) {
-        this.setState({currentPlayer: value})
-    }
-
-
-    handleClick(value) {
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[value]) {
-            return;
-        }
-
-        squares[value] = this.state.currentPlayer;
-        this.setState({squares: squares});
-    }
-
-    renderSquare(i) {
-        return <Square 
-            onClick={() => this.handleClick(i)}
-            value={this.state.squares[i]} 
-            changeCurrentPlayer={this.changeCurrentPlayer.bind(this)} 
-            currentPlayer={this.state.currentPlayer} />;
-    }
-
-    render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-        if (winner) {
-            status = 'Wygrywa: ' + winner;
-        } else {
-            status = 'Następny gracz: ' + (this.state.currentPlayer);
-        }
-
-        return (
-            <div>
-                <h2 className="status">
-                    <strong>{status}</strong>
-                </h2>
-                
-                <div className="board__container">
-                    {[...Array(9)].map((x, i) =>
-                        <div className="board__cell" key={i}>
-                            {this.renderSquare(i)}
-                        </div>
-                    )}
-                </div>
-            </div>
-        );
-    }
+    return (
+        <div className="login__container">
+                <Card className="login__box" sx={{ maxWidth: 600 }}>
+                <CardContent>
+                    <h2>Panel Logowania</h2>
+                    <FormGroup>
+                        <TextField id="login" label="Login" variant="standard" margin="dense" value={login} onChange={handleChangeLogin} />
+                    </FormGroup>
+                    <FormGroup>
+                        <TextField type="password" id="password" label="Hasło" variant="standard" margin="dense" value={password} onChange={handleChangePassword} />
+                    </FormGroup>
+                    <Box textAlign='center' mt={2}>
+                        <Button onClick={tryLogin} variant="contained" margin="dense">Zaloguj się</Button>
+                        <Button sx={{ ml: 1 }} variant="outlined" margin="dense">Zapomniałem hasła</Button>
+                    </Box>
+                </CardContent>
+            </Card>
+        </div>
+    );
 }
 
-class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
+const Main = () => {
+    return (
+        <div className="container">
+            <Container maxWidth="lg">
+                <App />
+            </Container>
+        </div>
+    );
 }
   
-
-function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-}
-
-// ========================================
   
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Game />);
-  
+root.render(<Main />);
